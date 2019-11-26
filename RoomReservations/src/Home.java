24d538d117ae44393fc5f74df1e05da160be148c
zzweb <zzweb@35.50.5.245>
2019-11-26 10:38:17 -0500

    Add edit event button

--- RoomReservations/src/Home.java
+++ RoomReservations/src/Home.java
@@ -6,25 +6,29 @@ public class Home extends JPanel {
 		JPanel createOrgPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		JPanel eventPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		JPanel createEventPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JPanel editEventPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		JPanel logoutPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		
 		JButton joinButton = new JButton("Join a Student Organization");
 		JButton createOrgButton = new JButton("Create a Student Organization");
 		JButton eventButton = new JButton("Upcoming Events");
 		JButton createEventButton = new JButton("Create an Event");
-		JButton logoutButton = new JButton("Logout");
+		JButton editEventButton = new JButton("Edit an Event");
+		JButton logoutButton = new JButton("Logout and Exit");
 		
 		public Home() {
 			joinPanel.add(joinButton);
 			createOrgPanel.add(createOrgButton);
 			eventPanel.add(eventButton);
 			createEventPanel.add(createEventButton);
+			editEventPanel.add(editEventButton);
 			logoutPanel.add(logoutButton);
 		
 			add(joinPanel);
 			add(createOrgPanel);
 			add(eventPanel);
 			add(createEventPanel);
+			add(editEventPanel);
 			add(logoutPanel);
 		}
 		
