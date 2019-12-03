953db4cda39022a429ff332a8810db3a6ffac3c5
Shelby McKay <shelbymckay1998@gmail.com>
2019-12-02 21:49:32 -0500

    Changed Home and added Membership class
    Added join and createOrg button listeners. Added code to join
    to call a instance of Membership. Created Membership to accept
    input and call the insert membership function. will need to test
    if the JFrame looks correct.

---
+++ RoomReservations/src/Membership.java
@@ -0,0 +1,57 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.Date;
+import javax.swing.*;
+
+public class Membership extends JFrame{
+	
+	public Membership(int stuID) {
+		setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+		setSize(300, 300);
+		
+		JPanel north = new JPanel(new FlowLayout());
+		JPanel west = new JPanel(new FlowLayout());
+		JPanel south = new JPanel();
+		
+		JTextField jtitle = new JTextField("Join a Student Organization");
+		north.add(jtitle);
+		
+		//this is all of the input, w
+		JLabel jorgid = new JLabel("Student Organization ID: ");
+		JTextField jorgidinput = new JTextField();
+		JLabel jmonth = new JLabel("Month joined: ");
+		JTextField jmonthinput = new JTextField();
+		JLabel jday = new JLabel("Day joined: ");
+		JTextField jdayinput = new JTextField();
+		JLabel jyear = new JLabel("Year joined: ");
+		JTextField jyearinput = new JTextField();
+		
+		west.add(jorgid);
+		west.add(jorgidinput);
+		west.add(jmonth);
+		west.add(jmonthinput);
+		west.add(jday);
+		west.add(jdayinput);
+		west.add(jyear);
+		west.add(jyearinput);
+		
+		JButton JSubmit = new JButton("Submit");
+		JSubmit.addActionListener(new ActionListener() {
+			@SuppressWarnings("deprecation")
+			public void actionPerformed(ActionEvent e) {
+				MySQLAccess msa = new MySQLAccess();
+				try {
+					msa.insertMembership(stuID, Integer.parseInt(jorgidinput.getText()), new Date(Integer.parseInt(jyearinput.getText()), Integer.parseInt(jmonthinput.getText()), Integer.parseInt(jdayinput.getText())));
+					
+				} catch (Exception ex) {	
+				}
+			}
+		});
+		south.add(JSubmit);
+		
+		add(north, BorderLayout.NORTH);
+		add(west, BorderLayout.WEST);
+		add(south, BorderLayout.SOUTH);
+	}
+	
+}
